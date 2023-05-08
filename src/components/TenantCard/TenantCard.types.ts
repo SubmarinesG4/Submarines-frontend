fb2021ba381724c036a2964035539496c7c26a14
davidemarzaro <58879664+davidemarzaro@users.noreply.github.com>
2023-05-08 15:03:17 +0200

    prova

---
+++ src/components/TenantCard/TenantCard.types.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+export type UseTenantCardOptions = {};
+
+export type UseTenantCardReturn = {};
+
+export type UseTenantCard = (options: UseTenantCardOptions) => UseTenantCardReturn;
+
+export type TenantCardProps = {};
+
+export type TenantCardView = React.FC<TenantCardProps>;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
+<<<<<<< Updated upstream
 import TranslationList from "@/components/TranslationList";
+=======
+import TenantCard from "@/components/TenantCard";
+import TranslationDrawer from "@/components/TranslationDrawer";
+import TranslationTable from "@/components/TranslationTable";
+>>>>>>> Stashed changes
 
 export default function Home() {
 	return (
 		<div>
+<<<<<<< Updated upstream
 			<TranslationList />
+=======
+			<TenantCard />
+>>>>>>> Stashed changes
 		</div>
 	);
 }
