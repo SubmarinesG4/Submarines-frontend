b351202aac26be40499aef343811c17ab8483ffc
davidemarzaro <58879664+davidemarzaro@users.noreply.github.com>
2023-05-08 15:06:50 +0200

    prova

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,10 @@
-<<<<<<< Updated upstream
-import TranslationList from "@/components/TranslationList";
-=======
-import TenantCard from "@/components/TenantCard";
-import TranslationDrawer from "@/components/TranslationDrawer";
-import TranslationTable from "@/components/TranslationTable";
->>>>>>> Stashed changes
 
+import TenantCard from "@/components/TenantCard";
 export default function Home() {
 	return (
 		<div>
-<<<<<<< Updated upstream
-			<TranslationList />
-=======
 			<TenantCard />
->>>>>>> Stashed changes
 		</div>
 	);
 }
+
