403da974d53433ea7426eba0f454f5c6dea91e26
Niccolò Fasolo <niccolofasolo@gmail.com>
2023-05-29 15:05:42 +0200

    fix bug

--- src/pages/Tenant.tsx
+++ src/pages/Tenant.tsx
@@ -9,40 +9,44 @@ import TenantUserTable from "@/components/TenantUserTable";
 import TenantDetails from "@/components/TenantDetails";
 
 export default function Tenant() {
-	let { id } = useParams();
-	if (!id) return <Navigate to="/" replace />;
-	return <TenantView tenantName={id} />;
+  let { id } = useParams();
+  if (!id) return <Navigate to="/" replace />;
+  return <TenantView tenantName={id} />;
 }
 
 function TenantView({ tenantName }: { tenantName: string }) {
-	const user = useAppSelector((state) => state.userSlice.user);
-	const [open, setOpen] = useState(false);
-	const { data, isLoading } = useGetTenantQuery({ id: tenantName });
+  const user = useAppSelector((state) => state.userSlice.user);
+  const [open, setOpen] = useState(false);
+  const { data, isLoading } = useGetTenantQuery({ id: tenantName });
 
-	function handleDrawer(open: boolean) {
-		setOpen(open);
-	}
+  function handleDrawer(open: boolean) {
+    setOpen(open);
+  }
 
-	const isAtLeastAdmin = user!.roles.includes("admin") || user!.roles.includes("super-admin");
-	if (isLoading) {
-		return <div>Loading</div>;
-	}
-	return (
-		<>
-			<Drawer anchor={"top"} open={open}>
-				{open && (
-					<Box sx={{ padding: "15px 30px" }}>
-						<InviteUserForm tenant={tenantName} closeDrawer={() => handleDrawer(false)} />
-					</Box>
-				)}
-			</Drawer>
-			<TenantDetails tenant={data} />
-			<TenantUserTable
-				tenant={tenantName}
-				users={data?.userList}
-				enableActions={isAtLeastAdmin}
-				onNewClick={() => handleDrawer(true)}
-			/>
-		</>
-	);
+  const isAtLeastAdmin =
+    user!.roles.includes("admin") || user!.roles.includes("super-admin");
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+  return (
+    <>
+      <Drawer anchor={"top"} open={open}>
+        {open && (
+          <Box sx={{ padding: "15px 30px" }}>
+            <InviteUserForm
+              tenant={tenantName}
+              closeDrawer={() => handleDrawer(false)}
+            />
+          </Box>
+        )}
+      </Drawer>
+      <TenantDetails tenant={data!} />
+      <TenantUserTable
+        tenant={tenantName}
+        users={data!.userList}
+        enableActions={isAtLeastAdmin}
+        onNewClick={() => handleDrawer(true)}
+      />
+    </>
+  );
 }
